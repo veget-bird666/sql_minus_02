03d7be329cf8c2af9200dfd62836c4609b05d2d1
veget-bird666 <23301164@bjtu.edu.cn>
2025-04-11 11:48:07 +0800

    Update table_manager.h

--- table_manager.h
+++ table_manager.h
@@ -1,4 +1,13 @@
 #ifndef TABLE_MANAGER_H
 #define TABLE_MANAGER_H
+#include "manager.h"
+#include "table_operations.h"
+
+class TableManager : Manager{
+public:
+    static void createTable(const CreateTableOperation* operation);
+    // static void dropTable(const DropTableOperation* operation);
+
+};
 
 #endif // TABLE_MANAGER_H
