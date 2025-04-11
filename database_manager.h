eecab303d3819f1f2ce2498100c444d9bdfe13b9
veget-bird666 <23301164@bjtu.edu.cn>
2025-04-11 11:42:30 +0800

    Update database_manager.h

--- database_manager.h
+++ database_manager.h
@@ -4,13 +4,16 @@
 #include "database_operations.h"
 #include "manager.h"
 
-//
+// test
 class DatabaseManager : Manager{
+private:
+
 public:
+    static QString currentDB;
     static void createDatabase(const CreateDatabaseOperation* operation);
     static void dropDatabase(const DropDatabaseOperation* operation);
     static void showDatabases();
-
+    static void useDatabase(const UseDatabaseOperation* operation);
 };
 
 
